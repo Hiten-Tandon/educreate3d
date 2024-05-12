91fd9e4026f7cedf4c70d3309eff3a1106b56adc
2salted <noamgraf123@gmail.com>
2024-05-12 13:12:45 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ export default function App() {
 
   return (
     <div className="bg-neutral-600">
+      <div className="flex-row absolute">
+        <div className="border-2 border-black">
+          <video width="300" height="200" controls={false} autoPlay={true}>
+            <source src="video/supercoolvideo.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className="border-2 border-black">
+          <video width="300" height="200" controls={false} autoPlay={true}>
+            <source src="video/supercoolvideo.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className="border-2 border-black">
+          <video width="300" height="200" controls={false} autoPlay={true}>
+            <source src="video/supercoolvideo.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
       <div className="flex items-center justify-center h-screen">
-        <div className="text-5xl">EduCreate 3D</div>
+        <div className="flex-row text-center">
+          <div className="text-5xl font-semibold p-2">EduCreate 3D</div>
+          <div className="text-3xl p-2">Unlocking Learning Through Interactive 3D models</div>
+        </div>
       </div>
     </div>
   )
